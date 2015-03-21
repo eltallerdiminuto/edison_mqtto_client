fb91725ad241ec97d89b3cbece2c15623eedab1a
Arnauti <rafa@elpulgardelpanda.com>
2015-03-21 11:41:51 +0100

    added test

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-var mqtt = require('mqtt');
-var time = require('time');
+// var mqtt = require('mqtt');
+// var time = require('time');
+//
+// var now = new time.Date();
+//
+// var broker = '176.58.96.63';
+// var port = 1883;
+// var message = now.toString() + ' - This is the message'
+// var topic = 'test/mqtt'
+//
+// var client = mqtt.connect(port, broker);
+//
+// client.publish(topic, message);
+// client.end();
 
-var now = new time.Date();
 
-var broker = '176.58.96.63';
-var port = 1883;
-var message = now.toString() + ' - This is the message'
-var topic = 'test/mqtt'
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://176.58.96.63');
 
-var client = mqtt.createClient(port, broker);
+client.on('connect', function () {
+  client.subscribe('presence');
+  client.publish('presence', 'Hello mqtt');
+});
 
-client.publish(topic, message);
-client.end();
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  client.end();
+});
