b626036d1b9072a66f6791b758cc8b4148362745
Arnauti <rafa@elpulgardelpanda.com>
2015-03-21 21:14:52 +0100

    added examples

--- emitter.js
+++ emitter.js
@@ -3,13 +3,11 @@ var client  = mqtt.connect('tcp://176.58.96.63');
 
 client.on('connect', function () {
   client.subscribe('general');
-  client.publish('general', 'Hello mqtt receiver!!');
+  // client.publish('general', 'Hello mqtt');
 });
 
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString());
-  client.end();
+  // client.end();
 });
-
-console.log("end!");
